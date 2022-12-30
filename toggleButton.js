49b74ca854cfd3ff71d1840faccbedbd56d6bcbf
JeanShyaka <shyakajeandedieu31@gmail.com>
2022-12-30 10:40:59 +0200

    toggleButton added

---
+++ toggleButton.js
@@ -0,0 +1,23 @@
+const toggleButton = document.querySelector('.toggle-button');
+const menu = document.querySelector('.menu');
+
+toggleButton.addEventListener('click', () => {
+    toggleButton.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+
+menu.addEventListener('click', ()=>{
+    document.querySelector('.toggle-button.active').classList.remove('active');
+    document.querySelector('.menu.active').classList.remove('active');
+})
+
+window.addEventListener('scroll', function(){
+    const nav = document.querySelector('.navigation');
+
+    if (window.scrollY > 0) {
+        nav.classList.add('scroll');
+      } else {
+        nav.classList.remove('scroll');
+      }
+});
+
